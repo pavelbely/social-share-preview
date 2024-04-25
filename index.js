bfea1c796a27e81fa3e1464b4a3af9010de1ec16
Pavel Bely <paviel.biely@gmail.com>
2024-04-25 13:48:32 +0300

    deploy

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
-app.set("views", __dirname + "/views");
+app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'pug');
-app.use(express.static(__dirname + "public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => res.send('Hello'));
 
